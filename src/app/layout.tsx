811ebd6173ce85b6875e125e2d812b5b8c776dd8
Vojtěch Smutný <smutny.vojta@gmail.com>
2026-03-01 23:47:02 +0100

    feat: Set default page title in metadata.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,9 @@ import { Figtree } from "next/font/google";
 
 const figtree = Figtree({ subsets: ["latin"], variable: "--font-sans" });
 
-export const metadata: Metadata = {};
+export const metadata: Metadata = {
+  title: "Online harmonogram"
+};
 
 export default function RootLayout({
   children,
