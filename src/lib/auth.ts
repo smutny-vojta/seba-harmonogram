461ba6fd07b498b78463296069f5c7a3b7161e4f
Vojtěch Smutný <smutny.vojta@gmail.com>
2026-03-03 02:31:55 +0100

    feat: Implement phone number authentication using better-auth with new Drizzle schemas and client-side configuration.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,21 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "@lib/db"; // your drizzle instance
+import { db } from "@lib/db";
+import { phoneNumber } from "better-auth/plugins"
 
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: "sqlite"
     }),
+    emailAndPassword: {
+        enabled: false,
+    },
+    plugins: [
+        phoneNumber({
+            sendOTP: ({ phoneNumber, code }, ctx) => {
+                // Implement sending OTP code via SMS
+                console.log(phoneNumber, code);
+            }
+        })
+    ]
 });
