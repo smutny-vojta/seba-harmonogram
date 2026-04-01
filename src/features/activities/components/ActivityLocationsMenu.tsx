28c2e426fa0b02296a698e8a32f07a55ebb0df5d
Vojtěch Smutný <smutny.vojta@gmail.com>
2026-04-01 13:44:35 +0200

    fix: update label text in ActivityLocationsMenu to reflect correct user group

--- src/features/activities/components/ActivityLocationsMenu.tsx
+++ src/features/activities/components/ActivityLocationsMenu.tsx
@@ -7,11 +7,11 @@ export default function ActivityLocationsMenu({ count }: { count: number }) {
       <div className="flex items-center gap-x-4">
         <div className="flex items-center gap-x-2">
           <div className="h-5 w-5 rounded-full bg-green-500"></div>
-          <span>Přístup mají všichni</span>
+          <span>Přístup pro všechny</span>
         </div>
         <div className="flex items-center gap-x-2">
           <div className="h-5 w-5 rounded-full bg-red-500"></div>
-          <span>Přístup mají pouze instři</span>
+          <span>Přístup pouze pro robinsony</span>
         </div>
       </div>
       <ActivityLocationsAddDialog />
